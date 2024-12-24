a098a7ecfe070b95a95e8958b913169f7813bd2d
Daniel Vela <daniel@veladan.org>
2024-12-24 14:58:48 +0100

    fix

--- create-ssl-server-cert.sh
+++ create-ssl-server-cert.sh
@@ -1,27 +1,32 @@
 openssl genrsa -out server.key 4096
 
-openssl req -new -key server.key -out server.csr -config <(cat <<EOF
+openssl req -new -newkey rsa:4096 -nodes -keyout server.key -out server.csr -config <(
+cat << EOF
 [req]
-default_bits = 4096
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
 prompt = no
-default_md = sha256
-distinguished_name = dn
-req_extensions = v3_req
-
-[dn]
-CN = gitlab.local
-emailAddress = daniel@veladan.org
-O = veladan
-L = zaragoza
-ST = zaragoza
+[req_distinguished_name]
 C = ES
-
+ST = Zaragoza
+L = Zaragoza
+O = veladan
+OU = IT
+CN = gitlab.local
 [v3_req]
+basicConstraints = CA:FALSE
+keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
-
 [alt_names]
 DNS.1 = gitlab.local
 EOF
 )
 
+
 openssl x509 -req -in server.csr -CA ~/.devca/ca.crt -CAkey ~/.devca/ca.key -CAcreateserial -out server.crt -days 1800 -sha256 -extfile v3.ext 
+
+
+
+# To check
+openssl x509 -in server.crt -text -noout | grep -A1 'Subject Alternative Name'
