328a15e6b24a317055b6d972a64a6da6fcb77544
Daniel Vela <daniel@veladan.org>
2024-12-24 14:19:46 +0100

    ssl certificate

---
+++ create-ssl-server-cert.sh
@@ -0,0 +1,35 @@
+openssl genrsa -out server.key 4096
+
+openssl req -new -key server.key -out server.csr -config <(cat <<EOF
+[req]
+default_bits = 4096
+prompt = no
+default_md = sha256
+distinguished_name = dn
+req_extensions = v3_req
+
+[dn]
+CN = gitlab.local
+emailAddress = daniel@veladan.org
+O = veladan
+L = zaragoza
+ST = zaragoza
+C = ES
+
+[v3_req]
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = gitlab.local
+EOF
+)
+
+openssl x509 -req -in server.csr -CA ~/.devca/ca.crt -CAkey ~/.devca/ca.key -CAcreateserial -out server.crt -days 1800 -sha256 -extfile <(cat <<EOF
+[req]
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = gitlab.local
+EOF
+)
+
